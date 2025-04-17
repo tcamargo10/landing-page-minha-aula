bf1ea4859f151a50379089e164f9f71d35646b51
Thiago Camargo <thiago.camargo@outlook.com>
2025-04-17 01:22:21 -0300

    feat: vercel json

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,22 @@ const nextConfig: NextConfig = {
   /* config options here */
   images: {
     domains: ["images.unsplash.com", "randomuser.me", "static.wixstatic.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+      },
+      {
+        protocol: "https",
+        hostname: "static.wixstatic.com",
+      },
+    ],
   },
+  distDir: ".next",
 };
 
 export default nextConfig;
